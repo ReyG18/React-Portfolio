78ac68a301716b27b1680334ea83f012b6fa4ec4
Rey G. <reyanna.garibay@gmail.com>
2024-07-23 21:55:24 -0500

    Grid implementation

--- React-Portfolio/src/components/header.jsx
+++ React-Portfolio/src/components/header.jsx
@@ -1,19 +1,25 @@
 import React from 'react';
 import Navigation from './navigation';
-import { AppBar, Toolbar, Typography, Box } from '@mui/material';
+import { AppBar, Toolbar, Typography, Container, Grid } from '@mui/material';
 
 function Header() {
 
   return(
     <AppBar position="fixed">
-      <Toolbar>
-        <Box sx={{ flexGrow: 1 }}>
-          <Typography variant="h6" component="div">
-            Reyanna Garibay
-          </Typography>
-        </Box>
-        <Navigation />
-      </Toolbar>
+      <Container>
+        <Toolbar>
+          <Grid container alignItems="center" justifyContent="space-between">
+            <Grid item>
+              <Typography variant="h6" component="div">
+                Reyanna Garibay
+              </Typography>
+            </Grid>
+          </Grid>
+          <Grid item>
+            <Navigation />
+          </Grid>
+        </Toolbar>
+      </Container>
     </AppBar>
   );
 };
